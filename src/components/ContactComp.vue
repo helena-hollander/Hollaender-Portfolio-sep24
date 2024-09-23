3f814eb431b97009065db538b7467100bd0db260
Helena Hollænder <helena.hollaender@gmail.com>
2024-09-23 21:00:46 +0200

    lidt mere mandag på grid i contact slim vers

--- src/components/ContactComp.vue
+++ src/components/ContactComp.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="h-screen overflow-hidden grid grid-cols-10 grid-rows-5">
-    <img src="../assets/images/tongue_web.png" alt="mouth" class=" col-start-2 col-end-6 row-start-1 row-end-6 justify-self-end self-center h-[80%]">
-    <div class="grid col-start-10 row-start-2 justify-self-end text-right pr-20 text-10xl font-display">
+  <div class="h-[120vh] overflow-hidden grid grid-cols-5 grid-rows-5 grid-flow-dense">
+    <img src="../assets/images/tongue_web.png" alt="mouth" class="col-start-1 col-end-7 row-start-3 row-end-9 self-center h-[100%]">
+    <div class="grid col-start-10 row-start-2 justify-self-end text-right text-2xl font-display">
       <a href="https://www.behance.net/helenahollnder" target="_blank" class="hover:underline">LinkedIn</a>
       <a href="https://www.behance.net/helenahollnder" target="_blank" class="hover:underline">BeHance</a>
       <a href="https://github.com/helena-hollander/portfoliosite-mappe" target="_blank" class="hover:underline">gitHub</a>
