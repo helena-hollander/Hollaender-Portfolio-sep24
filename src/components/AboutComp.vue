5a9e705de9f37c80d1a3bf06461294f67751fcf7
Helena Hollænder <helena.hollaender@gmail.com>
2024-09-23 20:41:10 +0200

    mandag_smal skærm fix hero og about med ny tekst

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="grid grid-cols-6 grid-rows-10 h-screen overflow-hidden">
-   <img src="../assets/images/tulipsWEB.png" alt="portrait" class="col-span-3 w-[90%] justify-self-end self-end">
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam eaque consequatur omnis, nulla qui expedita corporis sed tempora ipsam iste, consequuntur quae porro. Iusto soluta repellat consequatur blanditiis deserunt hic.</p>
-    </div>
+  <div class="grid grid-cols-6 grid-rows-6 h-[180vh] lg:overflow-hidden">
+   <img src="../assets/images/tulipsWEB.png" alt="portrait" class="col-span-6 row-start-6 w-[100%] justify-self-end self-end">
+   <p class="font-display row-start-3 col-start-5 justify-self-end self-end">hey!</p> 
+      <p class="font-display col-span-6 row-start-7 pt-6 px-2 pb-8">Welcome to this magical corner of the internet.<br>Here I like to create and share and connect with the world. ('Hello World!')<br>My name is Helena, I am 24 and currently studying Coded Design at DMJX in Copenhagen. My creative process is often messy and vibrant. I love to work with different mediums such as creative coding, illustration, painting, animation and 3D. Bold and beautiful stage design and quirky story-lines are a passion of mine.<br>My dream is to find like-minded people and create immersive digital experiences.<br><br>Please get in touch and let's get cooking!</p>
+    
   </div>
 </template>
--- src/components/HeroComp.vue
+++ src/components/HeroComp.vue
@@ -2,8 +2,8 @@
 </script>
 <template>
     <div class="w-sceen h-screen">
-        <div class="grid grid-cols-5 grid-rows-5 h-[100%] gap-4 ">
-            <h1 class="text-4xl col-start-1 col-span-3 row-start-1 row-end-2 justify-self-start self-center font-display pl-10 pb-20 pointer-events-none">h. hollænder</h1>
+        <div class="grid grid-cols-5 grid-rows-5 grid-flow-col-dense h-[100%] gap-4 ">
+            <h1 class="text-3xl col-span-4 justify-self-start self-start font-display pl-10 pt-4 pointer-events-none">h. hollænder</h1>
         </div>
         <img src="../assets/images/Ladybug2WEB.png" alt="ladybug2" class="absolute right-0 w-[90%]  lg:w-[80%] xl:w-[68%] top-0  lg:top-[-16%] xl:top-[-50%] pointer-events-none">
     
