e4774f5b1a20b44f8197b8510243d3072f9dcfa1
Helena Hollænder <helena.hollaender@gmail.com>
2024-09-24 15:36:38 +0200

    fri tirsdag

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="grid grid-cols-6 grid-rows-6 h-[180vh] lg:overflow-hidden">
-   <img src="../assets/images/tulipsWEB.png" alt="portrait" class="col-span-6 row-start-6 w-[100%] justify-self-end self-end">
+  <div class="grid grid-cols-6 grid-rows-6 xl:overflow-hidden h-[1350px] xl:h-screen">
+   <img src="../assets/images/tulipsWEB.png" alt="portrait" class="col-span-6 row-start-6 w-[100%] xl:w-[70%] xl:justify-self-start xl:self-center justify-self-end self-end z-10 pointer-events-none">
    <p class="font-display row-start-3 col-start-5 justify-self-end self-end">hey!</p> 
       <p class="font-display col-span-6 row-start-7 pt-6 px-2 pb-8">Welcome to this magical corner of the internet.<br>Here I like to create and share and connect with the world. ('Hello World!')<br>My name is Helena, I am 24 and currently studying Coded Design at DMJX in Copenhagen. My creative process is often messy and vibrant. I love to work with different mediums such as creative coding, illustration, painting, animation and 3D. Bold and beautiful stage design and quirky story-lines are a passion of mine.<br>My dream is to find like-minded people and create immersive digital experiences.<br><br>Please get in touch and let's get cooking!</p>
     
   </div>
 </template>
+
+
